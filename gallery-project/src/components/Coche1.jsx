abaf6a6f19b55a3d50c1234cef16055b00a0be5a
Christian Lopez <80645989+Chris9lopz@users.noreply.github.com>
2024-05-16 21:01:20 -0500

    add: create folders images and components, add first images

---
+++ gallery-project/src/components/Coche1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CocheImg1 from '../images/coche1.jpg'
+
+const Coche1 = () => {
+  return (
+    <div>
+      <img src={CocheImg1} alt="coche de img 1" />
+    </div>
+  )
+}
+
+export default Coche1
